c31744f64e38eb32f8743581c3cf4a79f6025ee4
hanhunh89 <146951587+hanhunh89@users.noreply.github.com>
2023-11-18 16:22:07 +0900

    Delete miniboard directory

--- miniboard/WEB-INF/classes/com/springmvc/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springmvc.repository;
-
-import com.springmvc.domain.User;
-
-public interface UserRepository {
-	void createUser(User User);
-	User getUser(String userId);
-	void deleteUser(String userId);
-}
--- miniboard/WEB-INF/views/header.jsp
+++
@@ -1,4 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=utf-8"%>
-<header>
-    <h1>Server : ${serverIp}</h1>
-</header>
